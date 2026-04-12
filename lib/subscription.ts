ae64ad5b51710bcf8df1d880ec38f901c28d34a9
agusgnwn96 <newmanfaheem@gmail.com>
2026-04-12 10:14:00 +0000

    ﻿Fix null assertion on stripeCurrentPeriodEnd
    Replace non-null assertion on optional getTime() call with an explicit null
    check to avoid NaN in date comparison when stripeCurrentPeriodEnd is null.

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -21,7 +21,8 @@ export const checkSubscription = async () => {
 
   const isValid =
     userSubscription.stripePriceId &&
-    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+    userSubscription.stripeCurrentPeriodEnd &&
+    userSubscription.stripeCurrentPeriodEnd.getTime() + DAY_IN_MS > Date.now();
 
   return !!isValid;
 };
